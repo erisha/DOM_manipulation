43dabcc651240eb8bebec0bf27a1d278ffef1528
Erisha Roach <forever.erisha@gmail.com>
2023-12-04 17:12:03 -0500

    part one finished

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 import "./styles.css";
 
-const mainE1 = document.querySelector('main');
-lo
+// Menu data structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+//Part 1
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+mainEl.classList.add("flex-ctr");
+
+//Part 2
+
+const topMenuEl = document.querySelector("#top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+// Part 3
+
+for (let linkObjects of menuLinks) {
+  const a = document.createElement("a");
+  a.setAttribute("href", linkObjects.href);
+  a.textContent = linkObjects.text;
+  topMenuEl.appendChild(a);
+}
