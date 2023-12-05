76a82f044fc22ef76853b5af596c9e40eddbe423
Erisha Roach <forever.erisha@gmail.com>
2023-12-05 17:43:32 -0500

    started working on part 2

--- script.js
+++ script.js
@@ -29,3 +29,36 @@ for (let linkObjects of menuLinks) {
   a.textContent = linkObjects.text;
   topMenuEl.appendChild(a);
 }
+
+//// ===== Part 2 Assignment =====\\\\\\
+
+const subMenuEl = document.querySelector('#sub-menu');
+subMenuEl.style.height = '100%';
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
+subMenuEl.classList.add('flex-around');
+// hide the subMunuEl
+subMenuEl.style.position = 'absolute';
+subMenuEl.style.top = '0';
+
+// get all a tags
+const topMenuLinks = document.querySelectorAll('a');
+console.log(topMenuLinks);
+
+// delegate top menu to listen for a click event
+topMenuEl.addEventListener('click', function (e) {
+  console.log(e);
+  e.preventDefault(); // stop the normal action of the a tag
+
+  if (e.target.localName !== 'a') {
+    return;
+  }
+  // toggle the active class on the anchor
+  e.target.classList.toggle('active');
+
+  for (let anchor of topMenuLinks) {
+    console.log(e.target.textContent, anchor.textContent);
+    if (e.target.textContent !== anchor.textContent) {
+      anchor.classList.remove('active');
+    }
+  }
+});
