28dda7e97d518a5577182cd5c9a070fad20cb31b
Erisha Roach <forever.erisha@gmail.com>
2023-12-05 13:53:02 -0500

    started working on part 2

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-import "./styles.css";
+import './styles.css';
 
 // Menu data structure
 var menuLinks = [
-  { text: "about", href: "/about" },
-  { text: "catalog", href: "/catalog" },
-  { text: "orders", href: "/orders" },
-  { text: "account", href: "/account" },
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '/catalog' },
+  { text: 'orders', href: '/orders' },
+  { text: 'account', href: '/account' },
 ];
 
 //Part 1
-const mainEl = document.querySelector("main");
-mainEl.style.backgroundColor = "var(--main-bg)";
-mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
-mainEl.classList.add("flex-ctr");
+const mainEl = document.querySelector('main');
+mainEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+mainEl.classList.add('flex-ctr');
 
 //Part 2
 
-const topMenuEl = document.querySelector("#top-menu");
-topMenuEl.style.height = "100%";
-topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
-topMenuEl.classList.add("flex-around");
+const topMenuEl = document.querySelector('#top-menu');
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add('flex-around');
 
 // Part 3
 
 for (let linkObjects of menuLinks) {
-  const a = document.createElement("a");
-  a.setAttribute("href", linkObjects.href);
+  const a = document.createElement('a');
+  a.setAttribute('href', linkObjects.href);
   a.textContent = linkObjects.text;
   topMenuEl.appendChild(a);
 }
